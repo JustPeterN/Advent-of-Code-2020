655addc84a867643fd8da6f5f5f5d58f4d529a20
JustPeterN <peternijessen@hotmail.com>
2020-12-01 12:36:04 +0100

    day1

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+def part1():
+    file1 = open('numbers', 'r')
+    lines = file1.readlines()
+
+    for x in lines:
+        for y in lines:
+            if int(y) + int(x) == 2020:
+                print(int(y) * int(x))
+
+def part2():
+    file1 = open('numbers', 'r')
+    lines = file1.readlines()
+
+    for x in lines:
+        for y in lines:
+            for z in lines:
+                if int(y) + int(x) + int(z) == 2020:
+                    print(int(y) * int(x) * int(z))
